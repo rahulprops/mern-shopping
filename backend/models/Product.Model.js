649ee8f53098afec114ff02a3f723875449819f2
rahulprops <rahulhack912@gmail.com>
2025-06-20 12:19:31 +0530

    usermodel create

---
+++ backend/models/Product.Model.js
@@ -0,0 +1,58 @@
+import mongoose, { Schema } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter product name"],
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please enter description"],
+    },
+    price: {
+      type: Number,
+      required: [true, "Please enter product price"],
+      maxLength: [7, "Price cannot exceed 7 digits"],
+    },
+    rating: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: [true, "Please enter product category"],
+    },
+    stock: {
+      type: Number,
+      required: [true, "Please enter product stock"],
+      default: 1,
+    },
+    numOfReviews: {
+      type: Number,
+      default: 0,
+    },
+    reviews: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+        rating: {
+          type: Number,
+          required: true,
+        },
+        comment: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+export default mongoose.model("Product", productSchema);
---
+++ backend/models/user.Model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter name"],
+      minLength: [3, "Name must be at least 3 characters"],
+      maxLength: [25, "Name must be less than 25 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter email"],
+      unique: true,
+      validate: [validator.isEmail, "Please enter a valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter password"],
+      minLength: [8, "Password should be at least 8 characters"],
+      select: false, // Exclude password from queries by default
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
