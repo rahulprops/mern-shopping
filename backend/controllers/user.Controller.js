4bacdc2331d9220997d8cf2fe02d161caee10c97
rahulprops <rahulhack912@gmail.com>
2025-06-20 21:17:26 +0530

    login user

--- backend/controllers/user.Controller.js
+++ backend/controllers/user.Controller.js
@@ -1,4 +1,6 @@
 import userModel from "../models/user.Model.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 export const Register = async (req, res) => {
   try {
@@ -43,5 +45,49 @@ export const Register = async (req, res) => {
    } catch (error) {
     // console.error("Register error:", error);
     res.status(500).json({ message: `server error ${error.message}`});
+  }
+};
+
+
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    
+    if (!email || !password) {
+      return res.status(400).json({ message: "Email and password are required" });
+    }
+
+    // Find user (with password)
+    const user = await userModel.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // 3. Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // 4. Generate JWT
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRE,
+    });
+
+    // 5. Send response (you can also set it in cookie if needed)
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: `Server error: ${error.message}` });
   }
 };
--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,15 @@ import express from 'express'
 import dotenv from 'dotenv'
 import dbConnect from './config/db.js';
 import UserRouter from './routers/User.Router.js';
+import bodyParser from 'body-parser';
+
 dotenv.config({path:"backend/.env"})
 const app =express()
  const port =process.env.PORT || 4000;
+
+ //! middlewares
+ app.use(bodyParser.json())
+ app.use(bodyParser.urlencoded({extended:true}))
 
  //! apis
  app.use("/api/user",UserRouter)
