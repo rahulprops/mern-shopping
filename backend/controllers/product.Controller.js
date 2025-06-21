2e760c1aea3cea4c020181226f408b5633db5c7f
rahulprops <rahulhack912@gmail.com>
2025-06-21 12:48:06 +0530

    get single product

--- backend/controllers/product.Controller.js
+++ backend/controllers/product.Controller.js
@@ -117,3 +117,24 @@ export const allProduct = async (req, res) => {
   }
 };
 
+//! single products
+
+
+export const singleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await ProductModel.findById(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ success: true, product });
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
